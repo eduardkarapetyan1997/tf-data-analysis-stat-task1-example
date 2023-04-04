423c4f25a6896fcbbb281fa2e0490213f49c1ca7
Eduard <eduardkarapetyan1997@mail.ru>
2023-04-04 16:55:30 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,5 +8,8 @@ def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    lmbd = np.mean(x) / 54
-    return lmbd # Ваш ответ
+    #lmbd = np.mean(x) / 54
+    #return lmbd # Ваш ответ
+    total_lamps = np.sum(x)
+    lambda_ = total_lamps / (len(x) * 54)
+    return lambda_
